8c239635687697b016b72fa63ff1137fa8d8f3e0
Md Ahmad <md.ahmad@globallogic.com>
2023-01-09 10:27:34 +0530

    changes have been added

--- aifrontend/src/Home.js
+++ aifrontend/src/Home.js
@@ -5,21 +5,13 @@ import banner from "./images/banner.jpg";
 import Duplicate from "./Duplicate";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHouse,
-  faWind,
-  faChartGantt,
-  faShuffle,
-  faChartPie,
-  faGlobe,
-  faEquals,
-  faRobot,
-} from "@fortawesome/free-solid-svg-icons";
+
 import SentimentAnalysis from "./SentimentAnalysis";
 import TextSummarization from "./TextSummarization";
 import WebScrapping from "./WebScrapping";
+import { screenRenderInfo, drawerOptions } from "./constants";
 
-function Home() {
+export const Home = () => {
   const [demostate, setDemostate] = useState("");
   const [menuOption, setMenuOption] = useState("Dashboard");
 
@@ -48,35 +40,22 @@ function Home() {
 
   const renderHomeCards = () => (
     <>
-     <Card
-        icon={faEquals}
-        onDemoClick={() => demoHandler("auto")}
-        link="/duplicate"
-        title="Matching & Pinning"
-        description="Solve duplicate records problem using machine learning in Matching & Pinning Process"
-      />
-      <Card
-        icon={faChartGantt}
-        onDemoClick={() => demoHandler("sentiment")}
-        link="/duplicate"
-        title="Sentiment Analysis"
-        description="Understand the sentiment of the customer and make informed business decisions."
-      />
-      <Card
-        icon={faChartPie}
-        onDemoClick={() => demoHandler("text")}
-        link="/duplicate"
-        title="Text Summarization"
-        description="Reduce the text size and create a summary of our textual data for business advantage."
-      />
-      <Card
-        icon={faGlobe}
-        onDemoClick={() => demoHandler("web")}
-        link="/duplicate"
-        title="Web Scrapping"
-        description="Scrape data from any website at scale and accuracy using best in class open source technology"
-      />
-     
+      {screenRenderInfo.map((item) => {
+        const { drawerOptions, iconName, title, description } = item;
+        return (
+          <>
+            {menuOption === drawerOptions && (
+              <Card
+                icon={iconName}
+                onDemoClick={() => demoHandler("auto")}
+                link="/duplicate"
+                title={title}
+                description={description}
+              />
+            )}
+          </>
+        );
+      })}
     </>
   );
 
@@ -100,88 +79,39 @@ function Home() {
     <div>
       <Header title={"My Todo List"} searchBar={false} />
       <div className="banner">
-        <img src={banner} ></img>
+        <img src={banner}></img>
         <div className="overlay">
           <h1>Artificial Intelligence Experience Hub</h1>
         </div>
       </div>
       <div className="content">
         <div className="tabs-v">
-          <input
-            id="rad1"
-            name="rad"
-            type="radio"
-            checked={menuOption === "Dashboard"}
-          />
-          <label
-            data-text="Dashboard"
-            for="rad1"
-            onClick={() => menuClick("Dashboard")}
-          ></label>
-          <span></span>
-          <FontAwesomeIcon icon={faHouse} className="faHouse" />
-          <input
-            id="rad2"
-            name="rad"
-            type="radio"
-            checked={menuOption === "NLP"}
-          />
-          <label
-            data-text="NLP"
-            for="rad2"
-            onClick={() => menuClick("NLP")}
-          ></label>
-          <span></span>
-          <FontAwesomeIcon icon={faWind} className="faHouse2" />
-          <input
-            id="rad3"
-            name="rad"
-            type="radio"
-            checked={menuOption === "Customer Analytics"}
-          />
-          <label
-            data-text="Customer Analytics"
-            for="rad3"
-            onClick={() => menuClick("Customer Analytics")}
-          ></label>
-          <span></span>
-          <FontAwesomeIcon icon={faChartGantt} className="faHouse3" />
-          <input
-            id="rad4"
-            name="rad"
-            type="radio"
-            checked={menuOption === "Auto ML"}
-          />
-          <label
-            data-text="Auto ML"
-            for="rad4"
-            onClick={() => menuClick("Auto ML")}
-          ></label>
-          <span></span>
-          <FontAwesomeIcon icon={faShuffle} className="faHouse4" />
-          <input
-            id="rad5"
-            name="rad"
-            type="radio"
-            checked={menuOption === "IOT"}
-          />
-          <label
-            data-text="IOT"
-            for="rad5"
-            onClick={() => menuClick("IOT")}
-          ></label>
-          <span></span>
-          <FontAwesomeIcon icon={faRobot} className="faHouse5" />
-          
-          <div className="tab-c">{renderHomeContent()}</div>
-          <div className="tab-c">{renderHomeContent()}</div>
-          <div className="tab-c">{renderHomeContent()}</div>
-          <div className="tab-c">{renderHomeContent()}</div>
-          <div className="tab-c">{renderHomeContent()}</div>
+          {drawerOptions.map((item, index) => {
+            const className = `faHouse${index+1}`
+            return (
+              <>
+                <input
+                  id={`rad${index + 1}`}
+                  name="rad"
+                  type="radio"
+                  checked={menuOption === item.name}
+                />
+                <label
+                  data-text={item.name}
+                  id={`rad${index + 1}`}
+                  onClick={() => menuClick(item.name)}
+                ></label>
+                <span></span>
+                <FontAwesomeIcon icon={item.iconName} className={className}/>
+              </>
+            );
+          })}
+
+          {drawerOptions.map(() => (
+            <div className="tab-c">{renderHomeContent()}</div>
+          ))}
         </div>
       </div>
     </div>
   );
 }
-
-export default Home;
---
+++ aifrontend/src/constants.js
@@ -0,0 +1,161 @@
+import {
+  faHouse,
+  faWind,
+  faChartGantt,
+  faShuffle,
+  faChartPie,
+  faGlobe,
+  faEquals,
+  faRobot,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const screenRenderInfo = [
+  {
+    iconName: faEquals,
+    title: "Matching & Pinning1",
+    description:
+      "Solve duplicate records problem using machine learning in Matching & Pinning Process.",
+    drawerOptions: "Dashboard",
+  },
+  {
+    iconName: faChartGantt,
+    title: "Sentiment Analysis1",
+    description:
+      "Understand the sentiment of the customer and make informed business decisions.",
+    drawerOptions: "Dashboard",
+  },
+  {
+    iconName: faChartPie,
+    title: "Text Summarization1",
+    description:
+      "Reduce the text size and create a summary of our textual data for business advantage.",
+    drawerOptions: "Dashboard",
+  },
+  {
+    iconName: faGlobe,
+    title: "Web Scrapping1",
+    description:
+      "Scrape data from any website at scale and accuracy using best in class open source technology.",
+    drawerOptions: "Dashboard",
+  },
+  {
+    iconName: faEquals,
+    title: "Matching & Pinning2",
+    description:
+      "Solve duplicate records problem using machine learning in Matching & Pinning Process.",
+    drawerOptions: "NLP",
+  },
+  {
+    iconName: faChartGantt,
+    title: "Sentiment Analysis2",
+    description:
+      "Understand the sentiment of the customer and make informed business decisions.",
+    drawerOptions: "NLP",
+  },
+  {
+    iconName: faChartPie,
+    title: "Text Summarization2",
+    description:
+      "Reduce the text size and create a summary of our textual data for business advantage.",
+    drawerOptions: "NLP",
+  },
+  {
+    iconName: faGlobe,
+    title: "Web Scrapping2",
+    description:
+      "Scrape data from any website at scale and accuracy using best in class open source technology.",
+    drawerOptions: "NLP",
+  },
+  {
+    iconName: faEquals,
+    title: "Matching & Pinning3",
+    description:
+      "Solve duplicate records problem using machine learning in Matching & Pinning Process.",
+    drawerOptions: "Customer Analytics",
+  },
+  {
+    iconName: faChartGantt,
+    title: "Sentiment Analysis3",
+    description:
+      "Understand the sentiment of the customer and make informed business decisions.",
+    drawerOptions: "Customer Analytics",
+  },
+  {
+    iconName: faChartPie,
+    title: "Text Summarization3",
+    description:
+      "Reduce the text size and create a summary of our textual data for business advantage.",
+    drawerOptions: "Customer Analytics",
+  },
+  {
+    iconName: faGlobe,
+    title: "Web Scrapping3",
+    description:
+      "Scrape data from any website at scale and accuracy using best in class open source technology.",
+    drawerOptions: "Customer Analytics",
+  },
+  {
+    iconName: faEquals,
+    title: "Matching & Pinning4",
+    description:
+      "Solve duplicate records problem using machine learning in Matching & Pinning Process.",
+    drawerOptions: "Auto ML",
+  },
+  {
+    iconName: faChartGantt,
+    title: "Sentiment Analysis4",
+    description:
+      "Understand the sentiment of the customer and make informed business decisions.",
+    drawerOptions: "Auto ML",
+  },
+  {
+    iconName: faChartPie,
+    title: "Text Summarization4",
+    description:
+      "Reduce the text size and create a summary of our textual data for business advantage.",
+    drawerOptions: "Auto ML",
+  },
+  {
+    iconName: faGlobe,
+    title: "Web Scrapping4",
+    description:
+      "Scrape data from any website at scale and accuracy using best in class open source technology.",
+    drawerOptions: "Auto ML",
+  },
+  {
+    iconName: faEquals,
+    title: "Matching & Pinning5",
+    description:
+      "Solve duplicate records problem using machine learning in Matching & Pinning Process.",
+    drawerOptions: "IOT",
+  },
+  {
+    iconName: faChartGantt,
+    title: "Sentiment Analysis5",
+    description:
+      "Understand the sentiment of the customer and make informed business decisions.",
+    drawerOptions: "IOT",
+  },
+  {
+    iconName: faChartPie,
+    title: "Text Summarization5",
+    description:
+      "Reduce the text size and create a summary of our textual data for business advantage.",
+    drawerOptions: "IOT",
+  },
+  {
+    iconName: faGlobe,
+    title: "Web Scrapping5",
+    description:
+      "Scrape data from any website at scale and accuracy using best in class open source technology.",
+    drawerOptions: "IOT",
+  },
+];
+
+export const drawerOptions = [
+  { name: "Dashboard", iconName: faHouse },
+  { name: "NLP", iconName: faWind },
+  { name: "Customer Analytics", iconName: faChartGantt },
+  { name: "Auto ML", iconName: faShuffle },
+  { name: "IOT", iconName: faRobot },
+];
